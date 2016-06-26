371181115e8e2fc50fa561af0060fd572db9ab56
kato1628 <kato@giftee.co>
2016-06-26 16:08:54 +0900

    binary search cpp.

---
+++ algorithm/ALDS1_4_B_BinarySearch.cpp
@@ -0,0 +1,20 @@
+// http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_4_B
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+
+int A[1000000], n;
+
+int main(){
+  cin >> n;
+  for(int i=0;i<n;i++) scanf("%d", &A[i]);
+  int q, k, sum = 0;
+  cin >> q;
+  for(int i=0;i<q;i++){
+    scanf("%d", &k);
+    if( *lower_bound(A, A+n, k) == k) sum++;
+  }
+  cout << sum << endl;
+  return 0;
+}
